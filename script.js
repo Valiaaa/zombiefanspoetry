14f3b1805e9f0eb5198cc7c0677b0f7d188cfd08
Valiaaa <valialcc@gmail.com>
2022-05-03 23:56:27 -0400

    !

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $. getJSON("poem.json")
     .done(function(data) {
       console.log(data);
-      let CID = data[0].poem;
+      let CID = data[0].CNID;
 
       $('.CNID').text(CID);
     });
