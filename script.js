47ee2daa0abfee93592b599589dac46c4d5be226
Valiaaa <valialcc@gmail.com>
2022-05-15 17:16:17 -0400

    !!

--- script.js
+++ script.js
@@ -8,11 +8,21 @@ $(document).ready(function(){
         let CID = data[i].CNID;
         let TIME = data[i].Time;
         let CContent = data[i].CNContent.replaceAll("\n","<br />");
+        let EID = data[i].ENID;
+        let ETIME = data[i].ENTime;
+        let EContent = data[i].ENContent.replaceAll("\n","<br />");
 
         $('.CNinfo').append("<div class='lines' id=" + i + '>' + "<div class='CNID'>" + CID + '</div>' + "<div class='Time'>" + TIME + '</div></div>');
 
         $('#' + i).click(function() {
           $('.content').html(CContent)
+        });
+
+
+        $('.ENinfo').append("<div class='lines' id=" + i + '>' + "<div class='CNID'>" + EID + '</div>' + "<div class='Time'>" + ETIME + '</div></div>');
+
+        $('#' + i).click(function() {
+          $('.ENcontent').html(EContent)
         });
       };
     });
